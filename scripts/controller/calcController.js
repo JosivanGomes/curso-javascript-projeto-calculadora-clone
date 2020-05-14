50b0373e63e167e04fd7268ee36a1752906016ce
JOSIVAN GOMES DA SILVA JUNIOR <josivansilvajunior@gmail.com>
2020-05-14 18:18:12 -0300

    mechendo no DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,21 +2,32 @@ class CalcController{
     constructor(){
         this._displayCalc = '0'
         this._currentDate
+        this.initialize()
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display")
+        let dateEl = document.querySelector("#data")
+        let hourEl = document.querySelector("#hora")
+        
+        displayCalcEl.innerHTML = "4567"
+        dateEl.innerHTML = "14/05/2020"
+        hourEl.innerHTML = "18:10"
     }
 
     get displayCalc(){
         return this._displayCalc
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor
+    set displayCalc(value){
+        this._displayCalc = value
     }
 
     get currentDate(){
         return this._currentDate
     }
 
-    set currentDate(valor){
-        this._currentDate = valor
+    set currentDate(value){
+        this._currentDate = value
     }
 }
