62bc51854c2e8e146bc43d0722d19752823d428a
JOSIVAN GOMES DA SILVA JUNIOR <josivansilvajunior@gmail.com>
2020-05-15 08:29:59 -0300

    Reconhecendo os botões e Add Events

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,30 +1,80 @@
 class CalcController{
     constructor(){
-        this._displayCalc = '0'
+        this._locale = 'pt-br'
+
+        this._displayCalcEl = document.querySelector("#display")
+        this._dateEl = document.querySelector("#data")
+        this._hourEl = document.querySelector("#hora")
+        
         this._currentDate
         this.initialize()
+        this.initButtonsEvents()
     }
 
     initialize(){
-        let displayCalcEl = document.querySelector("#display")
-        let dateEl = document.querySelector("#data")
-        let hourEl = document.querySelector("#hora")
-        
-        displayCalcEl.innerHTML = "4567"
-        dateEl.innerHTML = "14/05/2020"
-        hourEl.innerHTML = "18:10"
+        this.setDisplayDateTime()
+        setInterval(() => {
+            this.setDisplayDateTime()            
+        }, 1000)        
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false)
+        })
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn,'click drag', e =>{
+                console.log(btn.className.baseVal.replace('btn-',''))
+            })
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e =>{
+                btn.style.cursor = 'pointer'
+            })
+        })
+        
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        })
+        this.displayHour = this.currentDate.toLocaleTimeString(this._locale)
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value
+    }
+
+    get displayHour(){
+        return this._hourEl.innerHTML
+    }
+
+    set displayHour(value){
+        return this._hourEl.innerHTML = value
+    }
+
+
+
     get displayCalc(){
-        return this._displayCalc
+        return this._displayCalcEl.innerHTML
     }
 
     set displayCalc(value){
-        this._displayCalc = value
+        this._displayCalcEl.innerHTML = value
     }
 
     get currentDate(){
-        return this._currentDate
+        return new Date()
     }
 
     set currentDate(value){
